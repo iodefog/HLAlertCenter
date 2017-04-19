ce28dd32b2f409660fd999c6b31735610387bb3d
lihongli <honglili@sohu-inc.com>
2017-04-19 15:04:16 +0800

    Initial Commit

---
+++ TKAlertCenterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKAlertCenterDemo
+//
+//  Created by LHL on 2017/4/19.
+//  Copyright © 2017年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TKAlertCenterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TKAlertCenterDemo
+//
+//  Created by LHL on 2017/4/19.
+//  Copyright © 2017年 lihongli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
